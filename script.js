94071bbd90859a9500274b3d0b1cf49197b13500
Kollimarla Rishwanth <kollimarlarishwanth79@gmail.com>
2025-05-24 08:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault(); // Prevent form from submitting
 
+  const form = event.target;
+  let firstName = form.fname.value.trim();
+  let lastName = form.lname.value.trim();
+
+  if (!firstName && !lastName) {
+    alert("Please enter both first name and last name.");
+    return;
+  }
+
+  alert(firstName + " " + lastName);
 }
